af3244e137e64aeb997071fb322c3cf0c53e22d9
sai <2493saicharan@gmail.com>
2024-02-11 09:48:04 +0530

    GuessTheWord

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+Console.WriteLine("This is a guessing game");
+
+string str="",perfectGuess="sai";
+int chances_taken = 0, maximumChances = 3;
+bool outOf_Guesses = false;
+
+while (str != perfectGuess && !outOf_Guesses)
+{
+    if (chances_taken < maximumChances)
+    {
+        Console.Write("Guess the name : ");
+        str = Console.ReadLine();
+        Console.WriteLine("Try again");
+        chances_taken++;
+    }
+    else
+    {
+        outOf_Guesses=true;
+        Console.WriteLine("Restart the game");
+    }
+}
+
+if (outOf_Guesses)
+{
+    Console.WriteLine("You lose...");
+}
+else
+{
+    Console.WriteLine("You Win !!!");
+}
