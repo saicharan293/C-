2e0b063fda560d93f4cbc3ac70399f9b85fba007
sai <2493saicharan@gmail.com>
2024-02-11 09:46:10 +0530

    SwapUsingThirdVariable

---
+++ SwapWithVariable/SwapWithVariable/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Enter first value : ");
+int firstNumber=int.Parse(Console.ReadLine());//1
+Console.Write("Enter second value : ");
+int secondNumber=int.Parse(Console.ReadLine()),
+    thirdNumber=0;//2
+Console.WriteLine($"Before swap,  first number is {firstNumber} and second number is {secondNumber}");
+
+thirdNumber = firstNumber;
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+
+Console.WriteLine($"After swap, first number is {firstNumber}, second number is {secondNumber}");
+
+
