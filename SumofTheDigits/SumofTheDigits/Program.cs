2787ba7ff57f10201051c7f47194b16f9a6c9800
sai <2493saicharan@gmail.com>
2024-02-11 09:45:11 +0530

    sumOfTheDigits

---
+++ SumofTheDigits/SumofTheDigits/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//Sum Of the Digits
+//split the number, add the number through loop
+
+Console.Write("Enter the number : ");
+int number = int.Parse(Console.ReadLine()), r = 0, sum = 0, temp = number;
+
+while (number > 0)
+{
+    r = number % 10;
+    sum += r;
+    number /= 10;
+}
+
+Console.WriteLine($"For the given number {temp}, sum of the digits is {sum}");
+
